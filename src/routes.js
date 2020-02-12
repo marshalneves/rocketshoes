1a265c633e5719e155176ca7f463e11ef2306017
Marshal Neves <mcneves75@gmail.com>
2020-02-12 20:00:08 -0300

    Configurando rotas e excluindo arquivos desnecessários

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { React } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route />
+      <Route />
+    </Switch>
+  );
+}
