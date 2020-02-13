55fb501a2a538436bd1e66e94c2f9919a879beae
Marshal Neves <mcneves75@gmail.com>
2020-02-12 22:44:48 -0300

    WIP: Estilizando a Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import * as S from './styles';
 
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <S.ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt=""
+        />
+        <strong>R$129,90</strong>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            <span>ADICIONAR AO CARRINHO</span>
+          </div>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt=""
+        />
+        <strong>R$129,90</strong>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            <span>ADICIONAR AO CARRINHO</span>
+          </div>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt=""
+        />
+        <strong>R$129,90</strong>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            <span>ADICIONAR AO CARRINHO</span>
+          </div>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt=""
+        />
+        <strong>R$129,90</strong>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            <span>ADICIONAR AO CARRINHO</span>
+          </div>
+        </button>
+      </li>
+    </S.ProductList>
+  );
 }
 
 export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const ProductList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  list-style: none;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
+
+    img {
+      align-self: center;
+      max-width: 250px;
+    }
+  }
+`;
