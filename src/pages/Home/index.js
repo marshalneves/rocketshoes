2378d969a0fef541c43286c35bc70835255f77c6
Marshal Neves <mcneves75@gmail.com>
2020-02-15 13:39:18 -0300

    Buscando produtos da api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,46 @@
-import React from 'react';
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import * as S from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
-function Home() {
-  return (
-    <S.ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
-          alt=""
-        />
-        <strong>
-          Tênis bla bla djksah dskajhdsajk hdkjsahdjkasjkajkd djadsdjadjksh
-          adjhdkhj jdjdja jksja jkahjk bla bla
-        </strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </S.ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <S.ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </S.ProductList>
+    );
+  }
 }
-
-export default Home;
