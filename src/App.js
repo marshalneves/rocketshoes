c5f5997127461d622f6750658fcaee130bca099a
Marshal Neves <mcneves75@gmail.com>
2020-02-16 22:30:55 -0300

    redirect to cart on click button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import Routes from './routes';
@@ -7,16 +7,17 @@ import Routes from './routes';
 import GlobalStyle from './styles/global';
 import Header from './components/Header/index';
 import store from './store/index';
+import history from './services/history';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <ToastContainer autoClose={3000} />
         <GlobalStyle />
         <Header />
         <Routes />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
