aad79d15b813a2828d0bd8bcc2101356224206ef
Marshal Neves <mcneves75@gmail.com>
2020-02-16 21:51:26 -0300

    Separando actions

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,12 +1,31 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
-import api from '../../../services/api';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
-import { addToCartSuccess } from './actions';
+import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
+
+import { addToCartSuccess, updateAmount } from './actions';
 
 function* addToCart({ id }) {
   const response = yield call(api.get, `/products/${id}`);
 
-  yield put(addToCartSuccess(response.data));
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
+
+  if (productExists) {
+    // Atualiza apenas a qtd (amount)
+    const amount = productExists.amount + 1;
+    yield put(updateAmount(id, amount));
+  } else {
+    // Inclui o produto no carrinho
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
